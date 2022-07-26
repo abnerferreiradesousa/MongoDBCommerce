eb204c0059eba6174fb49bc19e780573c6c4d0d0
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-07-26 10:14:47 -0300

    [feat]: req 27 - done

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,6 +1,20 @@
 db.produtos.updateMany(
-  { valoresNutricionais: { $elemMatch: { percentual: { $gt: 20, $lt: 40 }, tipo: "sódio" } } },
-{ $push: { tags: "contém sódio" } },
+  { 
+    valoresNutricionais: { 
+      $elemMatch: { 
+        percentual: { 
+          $gt: 20, 
+          $lt: 40, 
+        }, 
+          tipo: "sódio", 
+        }, 
+      }, 
+    },
+{ 
+  $push: { 
+    tags: "contém sódio",
+  }, 
+},
 );
 
 db.produtos.find({ }, { nome: true, tags: true, _id: false });
